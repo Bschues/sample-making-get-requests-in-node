0cedc394b3af5ad1833ac83b503639601f50a19f
davegregg <davegregg@gmail.com>
2018-06-26 16:08:30 -0400

    Add module commentary

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const theFetchWay_AsyncAwait = require("./HTTPClients/fetchWay_AsyncAwait.js");
 const theAxiosWay_Then = require("./HTTPClients/axiosWay_Then.js");
 const theAxiosWay_AsyncAwait = require("./HTTPClients/axiosWay_AsyncAwait.js");
 
+// In the "requires" above, notice that I'm calling local files that I've written.
+// Look at how I'm doing this in each of those files. I'm setting "module.exports"
+// to an object (in this case a function), and this object is what gets imported
+// into this file and used below.
+//
+// Read: https://blog.risingstack.com/node-js-at-scale-module-system-commonjs-require/
+
 const categoryURI = "http://jservice.io/api/categories?count=1&offset=";
 
 theNativeWay(categoryURI + "0");
